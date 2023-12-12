e356bf27a446813f88283184d00fe978c88bbf10
BenWinsey <153075601+BenWinsey@users.noreply.github.com>
2023-12-12 16:12:13 +0000

    new commit

---
+++ 2023 Week 5/Test/2023 Week 5.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import numpy as np
+df  = pd.read_csv("C:/Users/BenjaminWinsey/Documents/GitHub/Preppin-Data-Challenges/2023 Week 5/Data Sources/PD 2023 Wk 1 Input (1).csv")
+print(df)
